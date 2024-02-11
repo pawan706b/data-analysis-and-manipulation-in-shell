bf9b181274a14b4b40635d305dcc08d226389019
pawan706b <145234266+pawan706b@users.noreply.github.com>
2024-02-10 20:31:16 -0400

    1.3 command bug fix

--- tablespace_and_tables.sh
+++ tablespace_and_tables.sh
@@ -7,4 +7,4 @@ awk 'NR>4 {arr[$1]++}END{max_count=0; max_tablespace=""; for (tablespace in arr)
 sort -r -n -k 3 tablespace_and_tables.csv | head -1 | awk '{largest_table=$2}; END{print "Largest table is", largest_table; print " with rows: ", $3}'
 
 # 1.3 - Largest table multiplying the number of rows with the avg_row_len
-awk 'NR>4 {product = $3 * $4; if (result > max_result) {max_result = result; table = $3} END {print "Highest Table:", table, print " with Value: ", max_result}' tablespace_and_tables.csv
+awk 'NR>4 {result = $3 * $4; if (result > max_result) {max_result = result; table = $3}} END {print "Highest Table:", table, "with Value:", max_result}' tablespace_and_tables.csv
